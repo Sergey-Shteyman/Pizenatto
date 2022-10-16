851d1b6a45ca3e642362ff5afa60b5f464940feb
Sergey_Shteyman <Shteiman10@gmail.com>
2022-10-16 14:08:44 +0300

    New reference to json. Add screenshots.

--- Pizenatto/Core/ServiceLayer/APIService.swift
+++ Pizenatto/Core/ServiceLayer/APIService.swift
@@ -24,7 +24,7 @@ final class APIService {
 extension APIService: APIServicable {
     
     func fetchPromotions(completion: @escaping (Result<[PromotionModel], Error>) -> Void) {
-        let urlString = "https://gist.githubusercontent.com/Sergey-Shteyman/2a7aa78ababc886c7a2c7f0a0da6b3ee/raw/c325b779f792ae5510e7a5fe17c4d0db3319ff1d/promotion.json"
+        let urlString = "https://gist.githubusercontent.com/Sergey-Shteyman/2a7aa78ababc886c7a2c7f0a0da6b3ee/raw/64ba7a2bb8f9c34d223128dfe681d21a8a0d1b33/promotion.json"
         networkService.request(urlString: urlString, completion: completion)
     }
     
